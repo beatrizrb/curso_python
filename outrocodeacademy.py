8b4e519b93194770005b7d9ab0e81f8617691e70
beatrizrb <beatrizrbergamo@gmail.com>
2023-06-14 16:51:06 -0300

    anew

--- outrocodeacademy.py
+++ outrocodeacademy.py
@@ -22,12 +22,14 @@ class School:
         self._noS = outro
     
     def __repr__(self):
+        # toda vez que eu printar o objeto aparece o abaixo
         return(f'A {self._level} school named {self._name} with {self._noS} students')
 
 
 a1 = School('oi', 'primario', 1000)
-print(a1.noS)
+#print(a1.noS)
+print(a1)
 print(repr(a1))
-a1.noS = 20000
-print(a1.noS)
-print(repr(a1))
+#a1.noS = 20000
+#print(a1.noS)
+#print(repr(a1))
